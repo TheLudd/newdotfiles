a4ea057e595e51ee292adc358ce8cd18460cef0e
Ludwig Magnusson <ludwig@mediatool.com>
2021-12-14 08:50:17 +0100

    more changes to lua

--- config/nvim/after/plugin/lspconfig.lua
+++ config/nvim/after/plugin/lspconfig.lua
@@ -25,7 +25,11 @@ end
 -- Set up completion using nvim_cmp with LSP source
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-lspconfig.tsserver.setup {on_attach=on_attach, filetypes={"typescript", "typescriptreact", "typescript.tsx"}, capabilities=capabilities}
+lspconfig.tsserver.setup {
+  on_attach=on_attach,
+  filetypes={"typescript", "typescriptreact", "javascript", "javascriptreact"},
+  capabilities=capabilities,
+}
 
 lspconfig.efm.setup {
   init_options={documentFormatting=true},
