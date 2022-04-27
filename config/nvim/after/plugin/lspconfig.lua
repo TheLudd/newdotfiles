51718d244711a53b7932c93514a2da91e33f68af
Ludwig Magnusson <ludwig@mediatool.com>
2022-04-27 16:12:52 +0200

    remove lua specific language server

--- config/nvim/after/plugin/lspconfig.lua
+++ config/nvim/after/plugin/lspconfig.lua
@@ -48,27 +48,3 @@ lspconfig.efm.setup {
 }
 
 lspconfig.eslint.setup {}
-
-local sumneko_root_path = CONFIG_HOME .. "/nvim/lua-language-server"
-local sumneko_binary = "lua-language-server"
-lspconfig.sumneko_lua.setup {
-  cmd={sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-  settings={
-    Lua={
-      runtime={
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version='LuaJIT',
-        -- Setup your lua path
-        path=vim.split(package.path, ';'),
-      },
-      diagnostics={
-        -- Get the language server to recognize the `vim` global
-        globals={'vim'},
-      },
-      workspace={
-        -- Make the server aware of Neovim runtime files
-        library={[vim.fn.expand('$VIMRUNTIME/lua')]=true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')]=true},
-      },
-    },
-  },
-}
