8c935092a29646287f4a4649e25cf475d2a5be86
Ludwig Magnusson <ludwig@mediatool.com>
2022-09-27 07:37:08 +0200

    add markdown preview plugin to vim

--- config/nvim/lua/plug.lua
+++ config/nvim/lua/plug.lua
@@ -45,4 +45,7 @@ packer.startup(function()
 
   -- git
   use 'f-person/git-blame.nvim'
+
+  -- markdown
+  use({ 'iamcco/markdown-preview.nvim', run = function() vim.fn['mkdp#util#install']() end })
 end)
