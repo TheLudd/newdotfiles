55e2daf43ec6f30341cca69b00e92b696ca4baee
Ludwig Magnusson <ludwig@mediatool.com>
2024-02-03 21:07:06 +0100

    chore(nvim): add basic options

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -1,1 +1,10 @@
 vim.cmd[[colorscheme tokyonight-night]]
+vim.opt.relativenumber = true -- set relative numbered lines
+vim.opt.number = true -- set relative numbered lines
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hidden = true -- Allow switching to buffers before save
+vim.opt.inccommand = 'split' -- Visualize preview of search/replace
+vim.opt.undofile = true -- enable persistent undo
+vim.opt.scrolloff = 0 -- always show n lines before/after the current one
