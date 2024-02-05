8d92868172d1fee6f1e46e24b9ad927e1b406786
Ludwig Magnusson <ludwig@mediatool.com>
2024-02-05 12:52:31 +0100

    configure expandtab

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -8,3 +8,7 @@ vim.opt.hidden = true -- Allow switching to buffers before save
 vim.opt.inccommand = 'split' -- Visualize preview of search/replace
 vim.opt.undofile = true -- enable persistent undo
 vim.opt.scrolloff = 0 -- always show n lines before/after the current one
+vim.opt.expandtab = true -- use spaces instead of tabs
+vim.opt.tabstop = 2 -- number of spaces that a <Tab> in the file counts for
+vim.opt.smartindent = true -- autoindent new lines
+vim.opt.shiftwidth = 2 -- number of spaces to use for autoindent
