73af260d3892607fbc5a66e800ad0e37c43682b5
Ludwig Magnusson <ludwig@mediatool.com>
2024-02-06 22:26:21 +0100

    fix exiting keymaps

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -1,7 +1,5 @@
 local keymap = vim.api.nvim_set_keymap
 
-local command = vim.api.nvim_create_user_command
-
 local opts = { noremap = true, silent = true }
 
 -- set map leader
@@ -28,12 +26,12 @@ keymap('n', '<Right>', 'xp', opts)
 keymap('n', '<Left>', 'hxph', opts)
 
 -- sane close and save commands
-command('X', 'x', {})
-command('Xa', 'xa', {})
-command('Q', 'q', {})
-command('Qa', 'qa', {})
-command('W', 'w', {})
-command('Wa', 'wa', {})
+vim.cmd('cnoreabbrev W w')
+vim.cmd('cnoreabbrev Wa wa')
+vim.cmd('cnoreabbrev X x')
+vim.cmd('cnoreabbrev Xa xa')
+vim.cmd('cnoreabbrev Q q')
+vim.cmd('cnoreabbrev Qa qa')
 
 -- open new file in same directory
 keymap('n', ',e', ':e <C-R>=expand("%:h") . "/" <CR>', opts)
