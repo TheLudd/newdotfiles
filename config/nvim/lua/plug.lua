9c2dd5139a138d16f52a2934e6e6d674db702b81
Ludwig Magnusson <ludwig@mediatool.com>
2024-12-16 10:26:36 +0100

    replace neodev with lazydev

--- config/nvim/lua/plug.lua
+++ config/nvim/lua/plug.lua
@@ -14,7 +14,17 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
   'neovim/nvim-lspconfig',
-  'folke/neodev.nvim',
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
 
   -- colorschemes
   'folke/tokyonight.nvim',
