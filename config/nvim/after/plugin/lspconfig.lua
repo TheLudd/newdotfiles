9bdd1a94bd68023b790e2d217582f58c4178816e
Ludwig Magnusson <ludwig@mediatool.com>
2025-11-04 16:18:39 +0100

    add amend-lsp

--- config/nvim/after/plugin/lspconfig.lua
+++ config/nvim/after/plugin/lspconfig.lua
@@ -6,3 +6,16 @@ end
 
 -- example to setup lua_ls and enable call snippets
 lspconfig.lua_ls.setup({ settings = { Lua = { completion = { callSnippet = 'Replace' } } } })
+
+require('lspconfig.configs').amendLsp = {
+  default_config = {
+    cmd = { 'amend-lsp', '--stdio' },
+    filetypes = { 'javascript', 'typescript' },
+    root_dir = function(fname)
+      return require('lspconfig.util').root_pattern('package.json', 'tsconfig.json', '.git')(fname) or vim.fn.getcwd()
+    end,
+    settings = {},
+  },
+}
+
+lspconfig.amendLsp.setup {}
