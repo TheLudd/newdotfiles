4faf31a94160d6124eeef4cca7f474f265cf2517
Ludwig Magnusson <ludwig@mediatool.com>
2025-11-16 09:54:29 +0100

    use specific node version path for copilot

--- config/nvim/after/plugin/copilot.lua
+++ config/nvim/after/plugin/copilot.lua
@@ -1,4 +1,6 @@
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 
+vim.g.copilot_node_command = '/usr/local/n/versions/node/22.21.1/bin/node'
+
 vim.api.nvim_set_keymap('i', '<C-h>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
