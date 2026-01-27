6460855e9da74e3c7380c002ccc1277a626cca73
Ludwig Magnusson <ludwig@mediatool.com>
2026-01-27 14:04:53 +0100

    fix tty config for non interactive shells

--- config/zsh/tty.zsh
+++ config/zsh/tty.zsh
@@ -1,4 +1,7 @@
 # Auto-start X on TTY 1-4
-if [[ $(echo $TTY | xargs basename | sed 's/tty//') -le 4 ]]; then
-    sx
+if [[ -n "$TTY" && "$TTY" == /dev/tty* ]]; then
+    tty_num=${TTY##/dev/tty}
+    if [[ "$tty_num" =~ ^[0-9]+$ && "$tty_num" -le 4 ]]; then
+        sx
+    fi
 fi
